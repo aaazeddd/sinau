7324431b688b8373bca6626f24c82273ff4623ce
Muhammad Azid Fadhil Nabhani <38772996+aaazeddd@users.noreply.github.com>
2020-12-30 11:13:52 +0700

    Create perkenalan.py
    import random = digunakan untuk mengambil secara acak pada []
    import time   = digunakan untuk mengambil waktu yang sedang berjalan di pc untuk menggunakan program
    time.sleep()  = digunakan untuk memberi jeda

---
+++ pyhton/hangman/perkenalan.py
@@ -0,0 +1,10 @@
+import random
+import time
+
+# ========= PERKENALAN ========== #
+print("\nSelamat Datang di permainan Hangman by aaazeddd")
+nama = input("Masukkan namamu : ")
+print("Halo " + nama + "! Semoga Beruntung!")
+time.sleep(2)
+print("Permainan segera dimulai!\n Persiapkan dirimu untuk bermain Hangman!")
+time.sleep(3)
